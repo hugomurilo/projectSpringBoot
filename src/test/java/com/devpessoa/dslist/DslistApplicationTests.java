0abac4766e8da67d9ceb75e784304b04b84c3366
Hugo Almeida <hugomurilo.almeida@gmail.com>
2023-07-11 17:28:43 -0300

    Project created

---
+++ src/test/java/com/devpessoa/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpessoa.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
