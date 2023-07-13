ff9769cff4007b0b42c3a6c27891924fd7c11a49
Hugo Almeida <hugomurilo.almeida@gmail.com>
2023-07-13 20:09:03 -0300

    GET list by id

--- src/main/java/com/devpessoa/dslist/services/GameService.java
+++ src/main/java/com/devpessoa/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devpessoa.dslist.dto.GameDTO;
 import com.devpessoa.dslist.dto.GameMinDTO;
 import com.devpessoa.dslist.entities.Game;
+import com.devpessoa.dslist.projections.GameMinProjection;
 import com.devpessoa.dslist.repositories.GameRepository;
 
 @Service
@@ -28,6 +29,13 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+		
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 		
 	}
